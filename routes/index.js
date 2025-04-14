2d86a7eaae31179b38a743e2166b7e05e9ac75c8
Mackewinsson P <mackewinsson@gmail.com>
2025-04-14 19:16:39 +0200

    fix en xml

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const fs = require("fs");
 const https = require("https");
 const xml2js = require("xml2js");
+const { log } = require("console");
 
 const parser = new xml2js.Parser({
   explicitArray: false,
@@ -112,66 +113,77 @@ router.post("/send-invoice", async (req, res) => {
 
   const xmlRequest = `
   <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:sum="https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroLR.xsd"
-    xmlns:sum1="https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd">
-    <soapenv:Header/>
-    <soapenv:Body>
-      <sum:RegFactuSistemaFacturacion>
-        <sum:Cabecera>
-          <sum1:ObligadoEmision>
-            <sum1:NombreRazon>${invoice.nombre}</sum1:NombreRazon>
-            <sum1:NIF>${invoice.nif}</sum1:NIF>
-          </sum1:ObligadoEmision>
-        </sum:Cabecera>
-        <sum:RegistroFactura>
-          <sum1:RegistroAlta>
-            <sum1:IDVersion>1.0</sum1:IDVersion>
-            <sum1:IDFactura>
-              <sum1:IDEmisorFactura>${invoice.nif}</sum1:IDEmisorFactura>
-              <sum1:NumSerieFactura>${invoice.numSerie}</sum1:NumSerieFactura>
-              <sum1:FechaExpedicionFactura>${
-                invoice.fecha
-              }</sum1:FechaExpedicionFactura>
-            </sum1:IDFactura>
-            <sum1:NombreRazonEmisor>${invoice.nombre}</sum1:NombreRazonEmisor>
-            <sum1:TipoFactura>F1</sum1:TipoFactura>
-            <sum1:DescripcionOperacion>${
-              invoice.descripcion || "Sin descripción"
-            }</sum1:DescripcionOperacion>
-            <sum1:Destinatarios>
-              <sum1:IDDestinatario>
-                <sum1:NombreRazon>${invoice.destNombre}</sum1:NombreRazon>
-                <sum1:NIF>${invoice.destNif}</sum1:NIF>
-              </sum1:IDDestinatario>
-            </sum1:Destinatarios>
-            <sum1:Desglose>
-              ${invoice.detalles
-                .map(
-                  (item) => `
-                <sum1:DetalleDesglose>
-                  <sum1:ClaveRegimen>${item.clave}</sum1:ClaveRegimen>
-                  <sum1:CalificacionOperacion>${item.calif}</sum1:CalificacionOperacion>
-                  <sum1:TipoImpositivo>${item.tipo}</sum1:TipoImpositivo>
-                  <sum1:BaseImponibleOimporteNoSujeto>${item.base}</sum1:BaseImponibleOimporteNoSujeto>
-                  <sum1:CuotaRepercutida>${item.cuota}</sum1:CuotaRepercutida>
-                </sum1:DetalleDesglose>`
-                )
-                .join("")}
-            </sum1:Desglose>
-            <sum1:CuotaTotal>${invoice.cuotaTotal}</sum1:CuotaTotal>
-            <sum1:ImporteTotal>${invoice.total}</sum1:ImporteTotal>
-          </sum1:RegistroAlta>
-        </sum:RegistroFactura>
-      </sum:RegFactuSistemaFacturacion>
-    </soapenv:Body>
-  </soapenv:Envelope>`;
+  xmlns:sum="https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroLR.xsd"
+  xmlns:sum1="https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <sum:RegFactuSistemaFacturacion>
+      <sum:Cabecera>
+        <sum1:ObligadoEmision>
+          <sum1:NombreRazon>MACKEWINSSON PALENCIA</sum1:NombreRazon>
+          <sum1:NIF>Z0706098A</sum1:NIF>
+        </sum1:ObligadoEmision>
+      </sum:Cabecera>
+      <sum:RegistroFactura>
+        <sum1:RegistroAlta>
+          <sum1:IDVersion>1.0</sum1:IDVersion>
+          <sum1:IDFactura>
+            <sum1:IDEmisorFactura>Z0706098A</sum1:IDEmisorFactura>
+            <sum1:NumSerieFactura>PRUEBA-0001</sum1:NumSerieFactura>
+            <sum1:FechaExpedicionFactura>14-04-2025</sum1:FechaExpedicionFactura>
+          </sum1:IDFactura>
+          <sum1:NombreRazonEmisor>MACKEWINSSON PALENCIA</sum1:NombreRazonEmisor>
+          <sum1:TipoFactura>F1</sum1:TipoFactura>
+          <sum1:DescripcionOperacion>Servicio de desarrollo API VeriFactu</sum1:DescripcionOperacion>
+          <sum1:Destinatarios>
+            <sum1:IDDestinatario>
+              <sum1:NombreRazon>MACKEWINSSON PALENCIA</sum1:NombreRazon>
+              <sum1:NIF>Z0706098A</sum1:NIF>
+            </sum1:IDDestinatario>
+          </sum1:Destinatarios>
+          <sum1:Desglose>
+            <sum1:DetalleDesglose>
+              <sum1:ClaveRegimen>01</sum1:ClaveRegimen>
+              <sum1:CalificacionOperacion>S1</sum1:CalificacionOperacion>
+              <sum1:TipoImpositivo>21</sum1:TipoImpositivo>
+              <sum1:BaseImponibleOimporteNoSujeto>100.00</sum1:BaseImponibleOimporteNoSujeto>
+              <sum1:CuotaRepercutida>21.00</sum1:CuotaRepercutida>
+            </sum1:DetalleDesglose>
+          </sum1:Desglose>
+          <sum1:CuotaTotal>21.00</sum1:CuotaTotal>
+          <sum1:ImporteTotal>121.00</sum1:ImporteTotal>
+          <sum1:Encadenamiento>
+            <sum1:PrimerRegistro>S</sum1:PrimerRegistro>
+          </sum1:Encadenamiento>
+          
+          <sum1:SistemaInformatico>
+            <sum1:NombreRazon>TEST</sum1:NombreRazon>
+            <sum1:NIF>Z0706098A</sum1:NIF>
+            <sum1:NombreSistemaInformatico>?ssss</sum1:NombreSistemaInformatico>
+            <sum1:IdSistemaInformatico>1</sum1:IdSistemaInformatico>
+            <sum1:Version>1</sum1:Version>
+            <sum1:NumeroInstalacion>1</sum1:NumeroInstalacion>
+            <sum1:TipoUsoPosibleSoloVerifactu>1</sum1:TipoUsoPosibleSoloVerifactu>
+            <sum1:TipoUsoPosibleMultiOT>?</sum1:TipoUsoPosibleMultiOT>
+            <sum1:IndicadorMultiplesOT>?</sum1:IndicadorMultiplesOT>
+          </sum1:SistemaInformatico>
+<sum1:FechaHoraHusoGenRegistro>${new Date().toISOString()}</sum1:FechaHoraHusoGenRegistro>          <sum1:TipoHuella>?</sum1:TipoHuella>
+          <sum1:Huella>?</sum1:Huella>
+        </sum1:RegistroAlta>
+      </sum:RegistroFactura>
+    </sum:RegFactuSistemaFacturacion>
+  </soapenv:Body>
+</soapenv:Envelope>
+  `;
 
   try {
     const response = await axios.post(
-      "https://prewww1.aeat.es/FacturacionSIF/ws/AltaLRFacturas", // Replace with actual AEAT test endpoint
+      "https://prewww1.aeat.es/wlpl/TIKE-CONT/ws/SistemaFacturacion/VerifactuSOAP",
       xmlRequest,
       {
-        headers: { "Content-Type": "text/xml" },
+        headers: {
+          "Content-Type": "text/xml",
+        },
         httpsAgent: httpsAgent,
       }
     );
@@ -180,6 +192,8 @@ router.post("/send-invoice", async (req, res) => {
     res.json({ success: true, aeatResponse: parsed });
   } catch (error) {
     console.error("AEAT Error:", error.message);
+    console.log(error);
+
     res
       .status(500)
       .json({ error: "Failed to send invoice", details: error.message });
